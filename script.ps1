09039d8dd5ef4994683be00e9f91e44097baea93
Ismael Brito <73890565+Ismael-Brito@users.noreply.github.com>
2024-02-06 17:28:04 -0300

    Delete script.ps1

--- script.ps1
+++
@@ -1,8 +0,0 @@
-function executarScriptPowerShell() {
-    var scriptPowerShell = "Add-Type -AssemblyName System.Windows.Forms;\n" +
-                            "while ($true) {\n" +
-                            "    $key = [System.Windows.Forms.MessageBox]::Show('Pressione uma tecla', 'Captura de Teclas', 'OKCancel', 'Information');\n" +
-                            "    Write-Output 'Tecla pressionada: ' + $key;\n" +
-                            "}";
-    window.location.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(scriptPowerShell);
-}
